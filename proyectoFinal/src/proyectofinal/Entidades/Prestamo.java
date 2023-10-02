53877b096c25694e8f5edf7f668cf4e74b5faba5
ladv1496 <135353493+ladv1496@users.noreply.github.com>
2023-10-02 17:49:32 -0300

    Define atributos y métodos para la clase Prestamo

--- proyectoFinal/src/proyectofinal/Entidades/Prestamo.java
+++ proyectoFinal/src/proyectofinal/Entidades/Prestamo.java
@@ -1,13 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectofinal.Entidades;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Date;
+
+
 public class Prestamo {
+    
+    private Date fechaInicio;
+    private Date fechaFin;
+    private Ejemplar ejemplar;
+    private Lector lector;
+    private boolean estado;
+    
+    //Constructores
+    
     public Prestamo(){}
+
+    public Prestamo(Date fechaInicio, Date fechaFin, Ejemplar ejemplar, Lector lector, boolean estado) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.ejemplar = ejemplar;
+        this.lector = lector;
+        this.estado = estado;
+    }
+    
+    //Getter & Setters
+    
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Ejemplar getEjemplar() {
+        return ejemplar;
+    }
+
+    public void setEjemplar(Ejemplar ejemplar) {
+        this.ejemplar = ejemplar;
+    }
+
+    public Lector getLector() {
+        return lector;
+    }
+
+    public void setLector(Lector lector) {
+        this.lector = lector;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    // toString 
+    
+    
+    @Override
+    public String toString() {
+        return "Prestamo{" + "fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", ejemplar=" + ejemplar + ", lector=" + lector + ", estado=" + estado + '}';
+    }
+    
+    
+    
 }
