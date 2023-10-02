5dcc5668a891ce4e2f337a757231a67469a03297
jarsarm <141675200+jarsarm@users.noreply.github.com>
2023-10-02 17:54:29 -0300

    Getter y Setter de las clases
    Se agregaron atributos y sus getter y setter para Autor Domicilio y Ejemplar

---
+++ proyectoFinal/src/proyectofinal/Entidades/Autor.java
@@ -0,0 +1,51 @@
+package proyectofinal.Entidades;
+
+public class Autor {
+    int id;
+    String nombre;
+    String apellido;
+    String nacionalidad;
+
+    public Autor() {
+    }
+
+    public Autor(int id, String nombre, String apellido, String nacionalidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+    
+}
