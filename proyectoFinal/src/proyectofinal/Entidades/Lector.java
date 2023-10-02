d676e4d983d5e7dc41739c3261a643ed5d7d96ca
brunotorresdias <torresdiasbruno6@gmail.com>
2023-10-02 17:37:42 -0300

    Inicializando atributos y métodos de la clase Lector.
    He creado tres métodos constructores. Uno vacío, en caso de que haya clases que requieran un nuevo lector pero que esté vacío para registrar sus datos después. Luego, he creado un constructor que pide diversos parámetros, los cuáles son: el número de socio, nombre, domicilio, e-mail y estado. Por último, añadí además un constructor que pide por parámetro todos los atributos mencionados en el constructor anterior a excepción del número de socio.

--- proyectoFinal/src/proyectofinal/Entidades/Lector.java
+++ proyectoFinal/src/proyectofinal/Entidades/Lector.java
@@ -9,5 +9,72 @@ package proyectofinal.Entidades;
  * @author Usuario
  */
 public class Lector {
+    private int nroSocio;
+    private String nombre;
+    private Object domicilio;
+    private String mail;
+    private boolean estado;
+    
     public Lector(){}
+    
+    public Lector(int numSocio, String nombreSocio, Object domicilioSocio, String mailSocio, boolean estadoSocio){
+    nroSocio = numSocio;
+    nombre = nombreSocio;
+    domicilio = domicilioSocio;
+    mail = mailSocio;
+    estado = estadoSocio;
+    }
+    
+    public Lector(String nombreSocio, Object domicilioSocio, String mailSocio, boolean estadoSocio){
+    nombre = nombreSocio;
+    domicilio = domicilioSocio;
+    mail = mailSocio;
+    estado = estadoSocio;
+    }
+
+    public int getNroSocio() {
+        return nroSocio;
+    }
+
+    public void setNroSocio(int nroSocio) {
+        this.nroSocio = nroSocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Object getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Object domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Socio: " + nroSocio + ", nombre: " + nombre + ", domicilio: " + domicilio + ", mail: " + mail + ", estado: " + estado;
+    }
+    
 }
