bc4c6ad2372582cf83a724fd1be25fa5e519ed83
ladv1496 <135353493+ladv1496@users.noreply.github.com>
2023-10-03 16:06:53 -0300

    Añadida Clase de Conexión a la Base de Datos.

--- proyectoFinal/src/proyectofinal/accesoDatos/Conexion.java
+++ proyectoFinal/src/proyectofinal/accesoDatos/Conexion.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package proyectofinal.accesoDatos;
 
-/**
- *
- * @author Usuario
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
 public class Conexion {
-    public Conexion(){}
+     private static final String URL="jdbc:mariadb://localhost/";
+     private static final String DB="biblioteca_publica";
+     private static final String USER="root";
+     private static final String PASS="";
+     private static Connection connection;
+            
+     private Conexion () {}
+
+     public static Connection getConexion() {
+     
+         if (connection ==null) {
+         
+             try {
+                 Class.forName("org.mariadb.jdbc.Driver");
+                 connection = DriverManager.getConnection(URL+DB,USER,PASS);
+                 
+             } catch (ClassNotFoundException ex) {
+                 JOptionPane.showMessageDialog(null,"Hubo un error al cargar los drivers");
+             } catch (SQLException ex) {
+                 JOptionPane.showMessageDialog(null,"Hubo un error de conexion con la base de datos");
+             }
+         }
+         return connection;
+     }
 }
